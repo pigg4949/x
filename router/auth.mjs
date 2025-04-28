bda0c680e3ddf9961f739493eb534951f337f9de
pigg4949 <pigg4949@gmail.com>
2025-04-28 10:50:05 +0900

    validator 적용

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,19 +1,45 @@
 import express from "express";
 import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("최소 4자 이상 입력")
+    .matches(/^[A-Za-z0-9]*$/)
+    .withMessage("특수문자는 사용 불가"),
+
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("최소 8자 이상 입력")
+    .matches(/^/),
+
+  validate,
+];
+
+const validateSignup = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("name을 입력"),
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
 
 // 회원가입
 // POST 방식
 // http://127.0.0.1:8080/auth/signup
 // json 형태로 입력 후 저장
-router.post("/signup", authController.signup);
+router.post("/signup", validateSignup, authController.signup);
 
 // 로그인
 // POST 방식
 // http://127.0.0.1:8080/auth/login
 // 아이디와 패스워드 입력시 해당 회원정보 출력
-router.post("/login", authController.login);
+router.post("/login", validateLogin, authController.login);
 
 // 로그인 유지
 
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,7 +1,14 @@
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 5 }).withMessage("최소 5자 이상 입력"),
+  validate,
+];
 
 // 모든 포스트 가져오기
 
@@ -20,13 +27,13 @@ router.get("/:id", postController.getPost);
 // POST 방식
 // http://127.0.0.1:8080/posts
 // json 형태로 입력 후 저장
-router.post("/", postController.createPost);
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // PUT 방식
 // http://127.0.0.1:8080/posts/:id
 // json 형태로 입력 후 저장
-router.put("/:id", postController.updatePost);
+router.put("/:id", validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // DELETE 방식
